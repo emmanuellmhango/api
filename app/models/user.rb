911515eea8f6d7328be95336d2703e79a04bedb3
emmanuel mhango <emmanuellmhango@gmail.com>
2023-06-30 19:03:03 +0400

    Updated the models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+  has_many :claims
+  has_many :categories
+  has_many :clients
+  
+  validates :username, presence: true, uniqueness: true
+  validates :password, presence: true, length: { minimum: 6 }
 end
