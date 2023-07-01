b990981781768c335ef0ae60986d448937a88942
emmanuel mhango <emmanuellmhango@gmail.com>
2023-07-02 03:23:31 +0400

    Remove authentications

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,2 @@
 class ApplicationController < ActionController::API
-  protect_from_forgery with: :null_session
-  before_action :set_default_format
-
-  private
-
-  def set_default_format
-    request.format = :json
-  end
 end
