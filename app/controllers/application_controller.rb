de83197843d623444185052a12c2d6f767b66e6c
emmanuel mhango <emmanuellmhango@gmail.com>
2023-07-02 03:16:43 +0400

    Update CSRF

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
-class ApplicationController < ActionController::Base
+class ApplicationController < ActionController::API
   protect_from_forgery with: :null_session
+  before_action :set_default_format
+
+  private
+
+  def set_default_format
+    request.format = :json
+  end
 end
