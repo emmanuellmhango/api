db3e47623f5b82a113bd1ed7c3b6c4fd84bd2ff4
emmanuel mhango <emmanuellmhango@gmail.com>
2023-07-02 04:44:01 +0400

    User response

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -27,7 +27,7 @@ class Api::V1::UsersController < ApplicationController
     @api_v1_user = User.new(api_v1_user_params)
 
     if @api_v1_user.save
-      render json: { status: 'Registration is successful' }
+      render json: { status: 'Registration is successful', code: 2200 }
     else
       render json: @api_v1_user.errors, status: :unprocessable_entity
     end
