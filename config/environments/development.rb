b036621f63807260a994f91744b36130c1841dff
emmanuel mhango <emmanuellmhango@gmail.com>
2023-07-04 22:53:29 +0400

    Modified users, clients and categories controller

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,7 +1,7 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.routes.default_url_options = {
-  host: 'http://localhost:3000'
+  host: 'https://api-production-3jvx.onrender.com'
 }
 
 Rails.application.configure do
