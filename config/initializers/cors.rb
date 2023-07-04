8cbe5a5fcea34f0bda9b018e0efd475ff0b01a92
emmanuel mhango <emmanuellmhango@gmail.com>
2023-07-05 02:57:45 +0400

    Added a development authorized access to the API

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3001'
+    origins 'http://localhost:3001', 'https://claims-app.onrender.com'
     resource '/api/v1/*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options],
