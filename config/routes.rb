7e7eee3c118e0079d338a439c6b661a003ff1f58
emmanuel mhango <emmanuellmhango@gmail.com>
2023-07-05 23:56:13 +0400

    Changed routing

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,8 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: 'json' } do
     namespace :v1 do
       get '/all_users', to: 'users#get_all_users'
-      resources :users do
-        resources :claims
-      end
+      resources :users
+      resources :claims
       resources :categories
       resources :clients
     end
