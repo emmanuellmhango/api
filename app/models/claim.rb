e07f24d1693ac1c21ec36f0f2a1a1956780b05a3
emmanuel mhango <emmanuellmhango@gmail.com>
2023-07-06 13:38:58 +0400

    Added image attachment in claim controller

--- app/models/claim.rb
+++ app/models/claim.rb
@@ -5,13 +5,13 @@ class Claim < ApplicationRecord
 
   has_many_attached :images
 
-  def image_urls
-    if images.attached?
-      images.map do |image|
-        Rails.application.routes.url_helpers.rails_blob_path(image, only_path: true)
-      end
-    else
-      self.images.attach(io: File.open(Rails.root.join(‘client’, ‘src’, ‘images’, ‘no_photo.jpeg’)), filename: ‘no_photo.jpeg’, content_type: “application/jpeg”)
-    end
-  end
+  # def image_urls
+  #   if images.attached?
+  #     images.map do |image|
+  #       Rails.application.routes.url_helpers.rails_blob_path(image, only_path: true)
+  #     end
+  #   else
+  #     self.images.attach(io: File.open(Rails.root.join(‘client’, ‘src’, ‘images’, ‘no_photo.jpeg’)), filename: ‘no_photo.jpeg’, content_type: “application/jpeg”)
+  #   end
+  # end
 end
--- app/serializers/claim_serializer.rb
+++ app/serializers/claim_serializer.rb
@@ -1,4 +1,4 @@
-class ClaimSerializer < ActiveModel::Serializer
-  include JSONAPI::Serializer
-  attributes :id, :comment, :location, :forwarded, :user_id, :client_id, :category_id, :images, :image_urls
-end
+# class ClaimSerializer < ActiveModel::Serializer
+#   include JSONAPI::Serializer
+#   attributes :id, :comment, :location, :forwarded, :user_id, :client_id, :category_id, :images, :image_urls
+# end
