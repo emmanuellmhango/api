13395df1813894f073d6d14245ae6493e0bf8f4e
emmanuel mhango <emmanuellmhango@gmail.com>
2023-07-07 02:02:30 +0400

    test sending one image

--- app/models/claim.rb
+++ app/models/claim.rb
@@ -4,13 +4,13 @@ class Claim < ApplicationRecord
   belongs_to :category
 
   has_one_attached :img_one
-  has_one_attached :img_two
+  # has_one_attached :img_two
 
   def img_one_url   
     Rails.application.routes.url_helpers.url_for(img_one) if img_one.attached?
   end  
   
-  def img_two_url   
-    Rails.application.routes.url_helpers.url_for(img_two) if img_two.attached?
-  end
+  # def img_two_url   
+  #   Rails.application.routes.url_helpers.url_for(img_two) if img_two.attached?
+  # end
 end
--- app/serializers/claim_serializer.rb
+++ app/serializers/claim_serializer.rb
@@ -1,4 +1,5 @@
 class ClaimSerializer < ActiveModel::Serializer
   include JSONAPI::Serializer
-  attributes :id, :location, :comment, :forwarded, :category_id, :user_id, :client_id, :img_one, :img_one_url, :img_two, :img_two_url
+  attributes :id, :location, :comment, :forwarded, :category_id, :user_id, :client_id, :img_one, :img_one_url 
+  # , :img_two, :img_two_url
 end
