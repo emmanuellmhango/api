207999576425ac83038f1d87b252551f26fa70c7
emmanuel mhango <emmanuellmhango@gmail.com>
2023-07-07 20:33:48 +0400

    Update claimserializer

--- app/serializers/claim_serializer.rb
+++ app/serializers/claim_serializer.rb
@@ -1,6 +1,10 @@
 class ClaimSerializer < ActiveModel::Serializer
   include JSONAPI::Serializer
-  attributes :id, :location, :comment, :forwarded, :category_id, :user_id, :client_id, :images, :image_urls
+  attributes :id, :location, :comment, :forwarded, :category_id, :user_id, :client_id, :created_at, :images, :image_urls
+
+  belongs_to :user
+  belongs_to :client
+  belongs_to :category
 
   def image_urls
     if object.images.attached?
