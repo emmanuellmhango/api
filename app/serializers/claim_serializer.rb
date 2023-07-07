15b184379cc8dca7271ac9127f3218248bc74020
emmanuel mhango <emmanuellmhango@gmail.com>
2023-07-07 16:03:49 +0400

    Fetching claims

--- app/serializers/claim_serializer.rb
+++ app/serializers/claim_serializer.rb
@@ -1,5 +1,4 @@
 class ClaimSerializer < ActiveModel::Serializer
   include JSONAPI::Serializer
   attributes :id, :location, :comment, :forwarded, :category_id, :user_id, :client_id, :images, :image_urls
-  
 end
