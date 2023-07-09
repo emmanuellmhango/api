68abfadaf36ca1e3b60da78cdbd6b3b8c35c6fae
emmanuel mhango <emmanuellmhango@gmail.com>
2023-07-10 02:04:30 +0400

    add password to usermanagement model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_09_105458) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_09_220314) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -92,6 +92,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_09_105458) do
     t.string "package"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password"
   end
 
   create_table "users", force: :cascade do |t|
