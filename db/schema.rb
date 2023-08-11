7fff83672df4eb2ab2f8297eaf8a4e5159d97da2
emmanuel mhango <emmanuellmhango@gmail.com>
2023-08-11 14:51:53 +0200

    Added role to the users model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_20_204802) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_11_124952) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -98,6 +98,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_20_204802) do
     t.string "dob"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "role"
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
