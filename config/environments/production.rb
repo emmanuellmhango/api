844ff760ddde37e293c4592540e31c3118db0c97
emmanuel mhango <emmanuellmhango@gmail.com>
2023-08-20 18:19:04 +0200

    Update production environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,7 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.routes.default_url_options = {
-  host: 'https://api-production-i5sf.onrender.com'
+  host: 'https://api-web-service-g48n.onrender.com'
 }
 
 Rails.application.configure do
