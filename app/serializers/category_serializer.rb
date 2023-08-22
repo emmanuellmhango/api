cf2ba418848eebf9cbc83d0122f1bf54e5212d1a
emmanuel mhango <emmanuellmhango@gmail.com>
2023-08-22 11:42:34 +0200

    Modified serializer class

--- app/serializers/category_serializer.rb
+++ app/serializers/category_serializer.rb
@@ -1,4 +1,4 @@
 class CategorySerializer
   include JSONAPI::Serializer
-  attributes :id, :name, :user_management_id, :created_at, :iconUrl
+  attributes :id, :name, :user_management_id, :description, :created_at, :iconUrl
 end
